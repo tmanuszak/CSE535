8e657e08ae7725ef25300f85d14886d783168b26
Aravind Hari Nair <aravindharinair@gmail.com>
2022-07-07 21:24:00 -0700

    Correct screen 3 and others

---
+++ Project/app/src/main/java/com/example/HelpMeHike/hiking_progress.java
@@ -0,0 +1,13 @@
+package com.example.HelpMeHike;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class hiking_progress extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hiking_progress);
+    }
+}
